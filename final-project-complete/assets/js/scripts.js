8187c728ddd0c321c5d4f88101723f3953a90980
AidenZChristopher <azchristophe@ualr.edu>
2025-05-10 17:58:01 +0000

    working on slideshow

---
+++ final-project-complete/assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true,
+      speed: 750,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+    
+
+
+});
